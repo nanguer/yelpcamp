e4b97bd86d1ffcae2d76c3f961674e9662245a58
nanguer <nngimenez1985@gmail.com>
2020-06-29 18:28:21 +0200

    first commit adding frontend

--- server/app.js
+++ server/app.js
@@ -62,5 +62,5 @@ app.use("/", indexRoutes);
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
-    console.log("The YelpCamp Server has started!");
+    console.log("The YelpCamp Server has started now!");
 })
